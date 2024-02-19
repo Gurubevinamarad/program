ae18cf4692a9fd3bffaf5581f64c9b92401b079d
Gururaj <bevinamaradgururaj@gmail.com>
2024-02-19 15:19:23 +0530

    Code for Simple calculator

---
+++ calculator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class calculator {
+
+    void calci() {
+        double num1 = 30, num2 = 10;
+
+        System.out.println("Num1=" + num1);
+        System.out.println("Num2=" + num2);
+
+        System.out.println("Addition=" + (num1 + num2));
+        System.out.println("Substraction=" + (num1 - num2));
+        System.out.println("Multiplication=" + (num1 * num2));
+        System.out.println("Division=" + (num1 / num2));
+    }
+}
+
+class arithmatic {
+    public static void main(String[] args) {
+        calculator z = new calculator();
+        z.calci();
+
+    }
+}
+
+
